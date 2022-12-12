29e8e0e8515ebc04e02c53cafefa374615659ed3
Xai Hang <xaihang12@gmail.com>
2022-12-12 15:05:48 -0600

    q5 added new parts arrival 25 number to the end of array with push method

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -18,10 +18,12 @@ console.log('3. Second supplyChange is:', supplyChanges[1]);
 //    array & console.log the value removed.
 let removedSupply = supplyChanges.pop();
 console.log('4. Removed item:', removedSupply);
-console.log('Updated supplyChanges list:', supplyChanges);
+console.log('Revised supplyChanges list after removed item:', supplyChanges);
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
+supplyChanges.push(25);
 console.log('5. Adding 25 to supplyChanges.');
+console.log('Revised supplyChange list after added item:', supplyChanges);
 
 // 6. Write a `for` loop that shows each value in the 'supplyChanges' array
 //    Use a console.log formatted as follows, where x is the value from the array
