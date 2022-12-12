bda3297cada5d7e99630fae896a0bdb233d5b7c2
Xai Hang <xaihang12@gmail.com>
2022-12-12 14:45:49 -0600

    q2 created variable called supplyChanges declared value in array with given element value in number

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -8,7 +8,8 @@ console.log('1. Number of partsNeeded:', partsNeeded);
 
 // 2. Create a variable call 'supplyChanges' set it to an array containing
 //    the following numbers: 3, 5, -6, 0, 7, 11
-console.log('2. Array of supplyChanges:');
+let supplyChanges = [3, 5, -6, 0, 7, 11];
+console.log('2. Array of supplyChanges:', supplyChanges);
 
 // 3. Console log the value of the second item in the 'supplyChanges' array
 console.log('3. Second supplyChange is:');
