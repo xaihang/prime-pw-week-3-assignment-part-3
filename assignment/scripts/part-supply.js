e129fde147d131900b931944d4e02dff28cbc4ea
Xai Hang <xaihang12@gmail.com>
2022-12-17 20:11:37 -0600

    comments codes and fixed bugs

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -16,14 +16,21 @@ console.log('3. Second supplyChange is:', supplyChanges[1]);
 
 // 4. The last item was added by mistake. Remove it from the 'supplyChanges'
 //    array & console.log the value removed.
+//comments:
+// use the pop() method to remove the last item from the array index
+// declared a new object name removedSupply, because pop will change the original array elements
+// then assigned current array (supplyChanges) and append it to the pop method
 let removedSupply = supplyChanges.pop();
-console.log('4. Removed item:', removedSupply);
-console.log('Revised supplyChanges list after removed item:', supplyChanges);
+console.log('4. Removed item:', removedSupply); // print out what we removed from the original array list (supplyChanges)
+console.log('Revised supplyChanges list after removed item:', supplyChanges); // print out the updated supplyChanges list after removal
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
-supplyChanges.push(25);
-console.log('5. Adding 25 to supplyChanges.');
-console.log('Revised supplyChange list after added item:', supplyChanges);
+//comments:
+//use the push() method to add to the last item of the array index
+//we do not need to declare a new objectname because it is simply changing the length of the array and returning the new length of it
+//take supplyChange append to push method; inside the parentheses push method add the parameters/argument in this case the 25 more parts that arrived, ie...push(25);
+supplyChanges.push(25); // accessing the object with 'dot' then push method with parameters (what we're pushing to end of array) inside parenthesis
+console.log('5. Adding 25 to supplyChanges.', supplyChanges); // print out the updated array with 25 at end of it
 
 // 6. Write a `for` loop that shows each value in the 'supplyChanges' array
 //    Use a console.log formatted as follows, where x is the value from the array
@@ -33,21 +40,21 @@ console.log('Revised supplyChange list after added item:', supplyChanges);
 console.log('6. Showing supplyChanges...');
 
 for (let i = 0; i < supplyChanges.length; i++) {
-  const x = supplyChanges[i];
+  const x = supplyChanges[i]; //`for` loop that shows each value in the 'supplyChanges' array
 
   if (x > 0) {
-    console.log(`Added ${x} parts.`);
+    console.log(`Added ${x} parts.`); //if it is a positive number (greater than 0), log 'Added x parts.'
   } else if (x === 0) {
-    console.log('No change.');
+    console.log('No change.'); //if the value is 0, log 'No Change.'
   } else {
-    console.log(`Removed ${x} parts.`);
+    console.log(`Removed ${x} parts.`); //if the value is negative, format the log as 'Removed x parts.'
   }
 }
 
 // STRETCH GOALS
 console.log('---  Stretch Goals  ---');
-// 7. Rewrite the `for` loop from #6 as a `for of` loop.
 
+// 7. Rewrite the `for` loop from #6 as a `for of` loop.
 console.log('7. Showing supplyChanges with "for of" loop');
 
 for (const supply of supplyChanges) {
@@ -62,14 +69,15 @@ for (const supply of supplyChanges) {
 
 // 8. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
+//comments:
+//  created a new variable called 'partsAvail' and assigned it to 0 number;
+// let starting point (index) begins at 0 index;
+//  keeping looping through each value in the supplyChanges array, adding the values as it loop
 let partsAvail = 0;
 for (let i = 0; i < supplyChanges.length; i++) {
   partsAvail += supplyChanges[i];
 }
 console.log('8. Total supplies available is:', partsAvail);
-
-// What is considered the parts available? Is it the supplyChanges or the partsNeeded?
-//Right now I am assuming it is the supplyChanges; and leaving out partsNeeded.
 
 // 9. We have a large stash of parts in our warehouse that we
 //    need to box up and get ready for shipment.
@@ -80,14 +88,15 @@ console.log('8. Total supplies available is:', partsAvail);
 
 console.log('9. Filling boxes with a "while" loop');
 
-let parts = 572;
-const boxHolds = 7;
-let box = 0;
+let parts = 572; // 572 parts in total
+const boxHolds = 7; // box holds 7 parts
+let box = 0; // need to determine how many box, right now we don't know so assigning it to 0 as a place holder
 
+// while parts is greater-than-or-equal to boxHolds, we will keep adding parts to boxes
 while (parts >= boxHolds) {
   parts -= boxHolds;
   box++;
 }
 
-console.log('Boxes that were filled:', box);
-console.log('Parts that are left over:', parts);
+console.log('Boxes that were filled:', box);  // log how many boxes were filled
+console.log('Parts that are left over:', parts); // log how many parts are left over
